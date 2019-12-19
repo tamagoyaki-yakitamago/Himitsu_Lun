b398925438fe8ea26521ddb23bf89059448cabff
tamagoyaki-yakitamago <yuki.takeda.6e0702@outlook.jp>
2019-12-19 23:04:47 +0900

    秘密分散でアップロードファイルの暗号化機能実装

--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,7 @@
 import responder
 
+from process import create_shares
+
 api = responder.API()
 
 
@@ -7,4 +9,41 @@ api = responder.API()
 class Index:
     def on_get(self, req, res):
         res.content = api.template("index.html")
+
+    async def on_post(self, req, res):
+        data = await req.media(format="files")
+        file = data["file"]
+        filename = file["filename"]
+        content = file["content"]
+        content_type = file["content-type"]
+        share_dict, tmp_filename = create_shares(filename, content)
+
+        if tmp_filename == "error":
+            message = """
+            ひみつるんにアップロードする際に、問題が発生しました。<br>
+            お手数ですが、時間をおいて再度アップロードしてください。<br>
+            """
+        else:
+            message = """
+            ひみつるんにアップロードが完了しました。<br>
+            データの復元をするには、次のURLにアクセスしてください。<br>
+            （データの復元には、3つのひみつるんきーのうち2つが必要です）<br>
+            <p class="url">http://localhost/%s</p>
+            <ul>
+            <li>ひみつるんきー：</li>
+            <li>#%s：%s</li>
+            <li>#%s：%s</li>
+            <li>#%s：%s</li>
+            </ul>
+            """ % (
+                tmp_filename,
+                "1",
+                share_dict["1"].decode("utf-8"),
+                "2",
+                share_dict["2"].decode("utf-8"),
+                "3",
+                share_dict["3"].decode("utf-8"),
+            )
+
+        res.content = api.template("index.html", message=message)
 
