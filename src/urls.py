ab848c37005585b5103b087b342a6b4b812a5ff8
tamagoyaki-yakitamago <yuki.takeda.6e0702@outlook.jp>
2019-12-20 23:44:36 +0900

    50MBまでアップロード対応

--- src/urls.py
+++ src/urls.py
@@ -16,34 +16,40 @@ class Index:
         filename = file["filename"]
         content = file["content"]
         content_type = file["content-type"]
-        share_dict, tmp_filename = create_shares(filename, content)
-
-        if tmp_filename == "error":
+        if len(content) > 10000000:
             message = """
             ひみつるんにアップロードする際に、問題が発生しました。<br>
             お手数ですが、時間をおいて再度アップロードしてください。<br>
             """
         else:
-            message = """
-            ひみつるんにアップロードが完了しました。<br>
-            データの復元をするには、次のURLにアクセスしてください。<br>
-            （データの復元には、3つのひみつるんきーのうち2つが必要です）<br>
-            <p class="url">http://localhost/%s</p>
-            <ul>
-            <li>ひみつるんきー：</li>
-            <li>#%s：%s</li>
-            <li>#%s：%s</li>
-            <li>#%s：%s</li>
-            </ul>
-            """ % (
-                tmp_filename,
-                "1",
-                share_dict["1"].decode("utf-8"),
-                "2",
-                share_dict["2"].decode("utf-8"),
-                "3",
-                share_dict["3"].decode("utf-8"),
-            )
+            share_dict, tmp_filename = create_shares(filename, content)
+
+            if tmp_filename == "error":
+                message = """
+                ひみつるんにアップロードする際に、問題が発生しました。<br>
+                お手数ですが、時間をおいて再度アップロードしてください。<br>
+                """
+            else:
+                message = """
+                ひみつるんにアップロードが完了しました。<br>
+                データの復元をするには、次のURLにアクセスしてください。<br>
+                （データの復元には、3つのひみつるんきーのうち2つが必要です）<br>
+                <p class="url">http://localhost/%s</p>
+                <ul>
+                <li>ひみつるんきー：</li>
+                <li>#%s：%s</li>
+                <li>#%s：%s</li>
+                <li>#%s：%s</li>
+                </ul>
+                """ % (
+                    tmp_filename,
+                    "1",
+                    share_dict["1"].decode("utf-8"),
+                    "2",
+                    share_dict["2"].decode("utf-8"),
+                    "3",
+                    share_dict["3"].decode("utf-8"),
+                )
 
         res.content = api.template("index.html", message=message)
 
